ec23e9ef9399bee29e305a9d9208e7b30ffd4796
lukash0lm <lukasholm@hotmail.com>
2022-10-07 14:28:50 +0200

    updated Controller to match .fxml file

--- src/main/java/com/example/tictactoe/Controller.java
+++ src/main/java/com/example/tictactoe/Controller.java
@@ -1,14 +1,54 @@
 package com.example.tictactoe;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 public class Controller {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Button bottomLeftSpace;
+
+    @FXML
+    private Button bottomMiddleSpace;
+
+    @FXML
+    private Button bottomRightSpace;
+
+    @FXML
+    private Button middleLeftSpace;
+
+    @FXML
+    private Button middleMiddleSpace;
+
+    @FXML
+    private Button middleRightSpace;
+
+    @FXML
+    private Label scoreLabel1;
+
+    @FXML
+    private Label scoreLabel2;
+
+    @FXML
+    private Label topLabel;
+
+    @FXML
+    private Button topLeftSpace;
+
+    @FXML
+    private Button topMiddleSpace;
+
+    @FXML
+    private Button topRightSpace;
+
+    @FXML
+    private Label turnLabel;
+
+    @FXML
+    void handleButtonClick(ActionEvent event) {
+
     }
+
 }
